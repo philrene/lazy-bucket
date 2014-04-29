89a253afb078ecc3ba8460f285b0982c291eac88
Phil Rene <philippe.rene@cenx.com>
2014-04-29 19:29:04 -0400

    Add lazy-bucker_example.clj for a global config for lazy-bucket

---
+++ lazy-bucket_example.clj
@@ -0,0 +1,6 @@
+;; move this out to ~/.lein/lazy-bucket.clj with proper config
+{:username "bob"
+ :password "bobpassword"
+ :reviewers ["adminusername" "architectusername"]
+ :team "this-co-rock"
+ :username-matches [{:name "Betty Boob" :username "thisisbettyboob"}]}
